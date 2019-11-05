0144eff2aabab5dc9e6a33f85ef589ddbc4b0781
John Rogala <johnrossrogala@gmail.com>
2019-11-05 17:31:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,18 +11,6 @@ end
 
 introduction("John")
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-introduction_with_language("John", "Ruby")
-
-def introduction_with_language_optional(name, languge = 'Ruby')
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-introduction_with_language_optional("John")
-
 
 
 
